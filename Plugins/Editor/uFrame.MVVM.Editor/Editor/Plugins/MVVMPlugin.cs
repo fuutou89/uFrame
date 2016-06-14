d88c2a25c605c851140634b202337ed955cc54d9
tan sd <tan sd>
2016-06-14 19:45:13 +0800

    Create MVVM plugin Database

---
+++ Plugins/Editor/uFrame.MVVM.Editor/Editor/Plugins/MVVMPlugin.cs
@@ -0,0 +1,12 @@
+namespace Invert.uFrame.MVVM {
+    using Invert.Core;
+    using Invert.Core.GraphDesigner;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    
+    
+    public class MVVMPlugin : MVVMPluginBase {
+    }
+}
